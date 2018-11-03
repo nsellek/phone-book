5bcb514e7a538a21ba5464e690a22fa9357a55a2
Nicholas Sellek <nick@seansellek.com>
2018-11-03 13:23:44 -0400

    Added fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+15.times do
+  User.create!({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.cell_phone,
+    email: Faker::Internet.email
+    })
+end
